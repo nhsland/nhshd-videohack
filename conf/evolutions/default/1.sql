8f15b8481c7091ad98a8f8596c69e34771f20028
Jay <Jay@DarkStar64>
2013-01-26 17:08:34 +0000

    added conf

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,30 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table appointment (
+  id                        bigint not null,
+  patient                   varchar(255),
+  start                     varchar(255),
+  end                       varchar(255),
+  title                     varchar(255),
+  body                      varchar(255),
+  constraint pk_appointment primary key (id))
+;
+
+create sequence appointment_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists appointment;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists appointment_seq;
+
