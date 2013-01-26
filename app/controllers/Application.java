36328082c5b57283bd435089c6e10871b0d4c562
Jay <Jay@DarkStar64>
2013-01-26 16:39:09 +0000

    added controller methods

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,15 @@
 package controllers;
 
+import java.util.List;
+
+import models.Appointment;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
+
+import play.data.DynamicForm;
+
 
 public class Application extends Controller {
   
@@ -12,6 +18,15 @@ public class Application extends Controller {
   }
   
   public static Result dashboard() {
+	  
+	  final DynamicForm form = form().bindFromRequest();
+	      final String name = form.get("username");
+	 //     return ok(index.render(String.format("You are %s, %s",surname, name)));
+	  
+	  	session("connected", name);
+	  
+	  	Logger.info(name);
+	  	
 	    return ok(dashboard.render());
   }
   
@@ -30,7 +45,33 @@ public class Application extends Controller {
   public static Result test(String start, String end, String title, String body) {
   
 	  Logger.info ( "start: " + start + " end: " + end );
+	  
+	  Appointment appointment = new Appointment();
+	  
+	  appointment.patient = session("connected");
+	  appointment.start = start;
+	  appointment.end = end;
+	  appointment.title = title;
+	  appointment.body = body;
+	  
+	  appointment.save();
+	  
+	  Logger.info( "patient"+ appointment.patient + "date" + appointment.start + appointment.end + appointment.title + appointment.body );
+	  
+	  
 	  return ok();
   
   }  
+  
+  public static Result list() {
+	  List<Appointment> appointments = Appointment.all();
+	  //Appointment appointment = new Appointment();
+	  
+	  Logger.info("lenth " + appointments.size());
+	  
+	return ok();
+	  
+  }
+  
+  
 }
---
+++ app/models/Appointment.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import play.db.ebean.*;
+import play.data.validation.Constraints.*;
+
+@Entity
+public class Appointment extends Model {
+
+	@Id
+	public Long id;
+
+	public String patient;
+	
+	public String start;
+	public String end;
+	
+	public String title;
+	public String body;
+	//public String email;
+	
+	
+	
+	public static Finder<Long, Appointment> find = new Finder(Long.class, Appointment.class);
+
+	public static List<Appointment> all() {
+		return find.all();
+	}
+
+	public static void create(Appointment appointment) {
+		appointment.save();
+	}
+
+	public static void delete(Long id) {
+		find.ref(id).delete();
+	}
+
+}
